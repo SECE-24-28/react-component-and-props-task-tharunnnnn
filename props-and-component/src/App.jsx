fe9f9aba71aa6af558d9c7961fab9574d739bd8b
tharunnnnn <tharunsanjay12@gmail.com>
2026-06-09 09:41:22 +0530

    completed  react props and components

---
+++ props-and-component/src/App.jsx
@@ -0,0 +1,13 @@
+import Card from './components/Card'
+
+function App() {
+  return (
+    <div>
+      <Card name="Surya" age={18} role="Developer" />
+      <Card name="Tharunn" age={19} role="Designer" />
+      <Card name="Vimal" age={18} role="Intern" />
+    </div>
+  )
+}
+
+export default App
---
+++ props-and-component/src/components/Card.jsx
@@ -0,0 +1,11 @@
+function Card({ name, age, role }) {
+  return (
+    <div>
+      <h3>{name}</h3>
+      <p>Age: {age}</p>
+      <p>Role: {role}</p>
+    </div>
+  )
+}
+
+export default Card
